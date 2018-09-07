1b440fbf8150996ac73873a72f4b9188db8ccb94
gusheng <2234839456@qq.com>
2018-09-07 14:52:53 +0800

    ceshi

--- user.js
+++ user.js
@@ -2,32 +2,44 @@
  * 对用户的一些操作 如增删改查之类的
  */
 var sql=require('./MySql');
+
+function basicsSqlOperation(sqltext,callback){
+    sql.query(sqltext,(value)=>{
+        callback(value)
+    })
+}
 exports.addUser=function(user){
     var sqltext=`INSERT INTO user VALUES (0, 
         "${user.name}",
         "${user.password}",
         "{}",
         "{}");`;
-    sql.query(sqltext,(value)=>{
+    basicsSqlOperation(sqltext,(value)=>{
         console.log(value)
     })
 }
 exports.removeUser=function(user){
     var sqltext=`DELETE FROM user WHERE name= "${user.name}" and password="${user.password}";`
-    sql.query(sqltext,(value)=>{
-        
+    basicsSqlOperation(sqltext,(value)=>{
+        console.log(value)
     })
-    
 }
 exports.updateUser=function(user){
     var sqltext=``
-    sql.query(sqltext,(value)=>{
-
+    basicsSqlOperation(sqltext,(value)=>{
+        console.log(value)
     })
 }
 exports.queryUser=function(user){
     var sqltext=``
+    basicsSqlOperation(sqltext,(value)=>{
+        console.log(value)
+    })
+}
+d
+exports.getUser=function(id){
+    var sqltext=`select * from user where id=${id}`
     sql.query(sqltext,(value)=>{
-        
+        console.log(value)
     })
 }
