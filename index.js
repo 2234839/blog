ecfb956f1cd433ce6b9f322f75f1e9a663ce9934
gusheng <2234839456@qq.com>
2018-09-07 15:26:48 +0800

    修改获取用户的函数

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var user=require("./user")
-for (let index = 0; index < 20; index++) {
-    user.addUser({
-        name:"阿强"+index,
-        password:Math.random().toString(36).substr(2)
-    })
-}
+// for (let index = 0; index < 20; index++) {
+//     user.addUser({
+//         name:"阿强"+index,
+//         password:Math.random().toString(36).substr(2)
+//     })
+// }
+console.log(user.getUser(1))
+
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var user=require('./user');
-user.addUser({
-    name:"阿强",
-    password:"love"
-})
+// user.addUser({
+//     name:"阿强",
+//     password:"love"
+// })
+console.log(user.getUser(1))
