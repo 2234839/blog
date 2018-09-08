e23eea7cd1144274154edb7603c28751c7e342d4
gusheng <2234839456@qq.com>
2018-09-08 19:58:10 +0800

    完善了一下user模块,简化了一些代码

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-var user=require("./user")
-var sql=require("./MySql")
-var log=console.log
-// for (let index = 0; index < 20; index++) {
-//     user.addUser({
-//         name:"阿强"+index,
-//         password:Math.random().toString(36).substr(2)
-//     })
-// }select * from user;
-
-// sql.query("select * from user",(d)=>{
-//     console.log(d)
-// })
-// user.addUser({
-//     id:1,
-//     name:"666",
-//     password:"kkk",
-//     data:{
-//         a:"ddd"
-//     },
-//     msg:{
-//         a:9999
-//     }
-// })
-for (let index = 30; index < 60; index++) {
-
-    user.removeUser(index)    
-}
+var user = require("./user")
+var log = console.log
+user.updateUser({
+    id: 60,
+    name: "000",
+    password: "kkk",
+    data: {
+        a: "ddd"
+    },
+    msg: {
+        a: "sssssssssssss"
+    }
+})
--- user.js
+++ user.js
@@ -1,58 +1,60 @@
 /**
  * 对用户的一些操作 如增删改查之类的
  */
-var sql=require('./MySql');
+var sql = require('./MySql');
 // user对象的结构
-var user={
-    id:0,
-    name:"ddd",
-    password:"string",
-    data:{},
-    msg:{}
-}
+// var user={
+//     id:0,
+//     name:"ddd",
+//     password:"string",
+//     data:{},
+//     msg:{}
+// }
 
 /**
  * 添加一个用户到数据库中
- * @param {user}} user 
+ * @param {user} user 包含基本信息的user对象
+ * @param {Function} callback 操作成功则返回id否则返回数据库的返回信息
  */
-exports.addUser=function(user){
-    //模块会将json直接转为 “a=0” 这种形式的 ，所以需要将他们序列化一下
-    sql.query('INSERT INTO user VALUES (?,?,?,?,?)',[0,user.name, user.password,JSON.stringify(user.data),JSON.stringify(user.msg)],(results)=>{
-        console.log(results)
+exports.addUser = function (user, callback) {
+    //模块会将json对象直接转为 “a=0” 这种形式的 ，所以需要将他们序列化一下
+    sql.query('INSERT INTO user VALUES (?,?,?,?,?)', [0, user.name, user.password, JSON.stringify(user.data), JSON.stringify(user.msg)], (results) => {
+        querySuccess(results)
+        if (results.affectedRows == 1)
+            callback(results.insertID)
+        else
+            callback("添加账户失败", results)
     })
 }
 /**
  * 移除一个用户
  * @param {number} id
  */
-exports.removeUser=function(id){
-    sql.query('DELETE FROM user WHERE id=?', [id],(results)=>{
-        console.log(results)
+exports.removeUser = function (id, callback) {
+    sql.query('DELETE FROM user WHERE id=?', [id], (results) => {
+        console.log("删除用户", results)
+        if (results.affectedRows == 1)
+            callback("注销成功")
+        else
+            callback("注销失败", results)
     })
 }
 /**
  * 更新用户的一些资料
- * @param {objiect}} user 完整的用户对象
+ * @param {objiect}} user 完整的用户对象需要包含id属性
  */
-exports.updateUser=function(user){
-    console.log(user)
-    var sqltext=`UPDATE user SET 
-        name="${user.name}",
-        password="${user.password}",
-        data="${JSON.stringify(user.data)}",
-        msg="${JSON.stringify(user.msg)}" WHERE id=${user.id};`
-    basicsSqlOperation(sqltext,(value)=>{
-        console.log(value)
+exports.updateUser = function (user) {
+    sql.query('UPDATE user SET name=?,password=?,data=?,msg=? where id=?', [user.name, user.password, JSON.stringify(user.data), JSON.stringify(user.msg), user.id], (results) => {
+        console.log("更新用户数据", results)
     })
 }
 /**
  * 查询昵称为name的用户返回一个包含这些用户的数组
  * @param {string} name 
  */
-exports.queryUser=function(name){
-    var sqltext=`select * from user where name=${name}`
-    basicsSqlOperation(sqltext,(value)=>{
-        console.log(value)
+exports.queryUser = function (name) {
+    sql.query('select * from user where name=', [name], (results) => {
+        console.log("查询用户", results)
     })
 }
 /**
@@ -60,9 +62,22 @@ exports.queryUser=function(name){
  * @param {number} id 要查询用户的id
  * @param {Function} callback 回调函数
  */
-exports.getUser=function(id,callback){
-    var sqltext=`select * from user where id=${id}`
-    sql.query(sqltext,(value)=>{
+exports.getUser = function (id, callback) {
+    var sqltext = `select * from user where id=${id}`
+    sql.query(sqltext, (value) => {
         callback(value[0])
     })
+}
+/**
+ * 
+ * @param {results} results 数据库返回的消息
+ * @param {Function} callback 回调函数
+ * @param {strign} okMsg 成功之后返回的消息
+ * @param {array} failMsg 失败之后返回的数组
+ */
+function querySuccess(results,callback,okMsg,failMsg) {
+    if (results.affectedRows == 1)
+        callback(okMsg)
+    else
+        callback(failMsg)
 }
