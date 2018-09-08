5a2070f33993761ac8091da632b65d898a4a3f43
gusheng <2234839456@qq.com>
2018-09-09 01:01:35 +0800

    完善了一下webServer,原来的那个太简陋了

--- webServer/server/server.js
+++ webServer/server/server.js
@@ -7,23 +7,28 @@ var http = require("http");
 var url = require("url");
 var fs = require("fs");
 
+var contentType=require("./ContentType")
+
 // Create a server and start
 var server = http.createServer(function (request, response) {
     var oUrl = url.parse(request.url);
     var sPath = oUrl.pathname;
     var sRoot = serverConfig.config.web_root;
+    
+    console.log(request.url,oUrl.pathname,sRoot + sPath)
     // If request the home directory, redirect to welcome page
     if (sPath === "/" || /^\/*\/$/.test(sPath)) {
         sPath = serverConfig.config.welcome_page;
     }
+    response.writeHead(200, { 'Content-Type': contentType.query(sPath.substring(sPath.lastIndexOf('.'))) });
     // Read server resource content and output to browser
-    fs.readFile(sRoot + sPath, "utf-8", function (err, data) {
+    //这里去掉它原来的 , "utf-8" encoding 参数  ,因为使用了这个参数则无法发送图片
+    fs.readFile(sRoot + sPath, function (err, data) {
         if(err) {
             console.log(err);
-            response.writeHead(404);
             // Redirect to 404 page
-            console.log(sRoot + sPath)
             fs.readFile(sRoot + serverConfig.config.error_page['404'], "utf-8", function(err,data){
+                response.writeHead(301,{'Location':''}); 
                 response.end(data);
             });
             return;
