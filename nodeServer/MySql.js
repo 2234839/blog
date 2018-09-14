8d43bbd944da8d9d1a39f5187cbfc437fe6df51b
gusheng <2234839456@qq.com>
2018-09-15 00:13:15 +0800

    创建了一个nodeServerConfig配置模块,用来存储nodeserver的配置

--- nodeServer/MySql.js
+++ nodeServer/MySql.js
@@ -1,11 +1,9 @@
 //先在服务器上创建一个数据库    create database Blog;
 var mysql=require('mysql')
-var connection = mysql.createConnection({
-  host     : '39.108.235.79',
-  user     : 'root',
-  password : '987456321',
-  database : 'Blog'
-});
+const config=require('./nodeServerConfig').config
+
+var connection = mysql.createConnection(config.mysql);
+
 connection.connect();
 /**
  * 提供对sql语句的编码，防范sql注入
---
+++ nodeServer/nodeServerConfig.js
@@ -0,0 +1,8 @@
+exports.config = {
+    mysql: {
+        host: '39.108.235.79',
+        user: 'root',
+        password: '987456321',
+        database: 'Blog'
+    }
+}
