f4698f3c1bb1f2256648033752809d34c32a4fe3
SZL <2234839456@qq.com>
2018-10-17 14:33:39 +0800

    添加的双因素验证的qr码的接口

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -17,34 +17,3 @@ async function test() {
 test()
 
 
-var tfa = require('2fa');
-tfa.generateKey(31, function (err, key) {
-    console.log(key);
-    // crypto secure hex key with 32 characters
-    // generate crypto-secure backups codes in a user-friendly pattern
-    // tfa.generateBackupCodes(num, pattern (optional), cb)
-    tfa.generateBackupCodes(8, 'xxxx-xxxx-xxxx', function (err, codes) {
-        // [ '7818-b7b8-c928', '3526-dc04-d3f2', 'be3c-5d9f-cb68', ... ]
-        console.log(codes);
-        // these should be sent to the user, stored and checked when we get a 2fa code
-    });
-
-    // generate a google QR code so the user can save their new key
-    // tfa.generateGoogleQR(name, accountname, secretkey, cb)
-    tfa.generateGoogleQR('Company', 'email@gmail.com', key, function (err, qr) {
-        // data URL png image for google authenticator
-        console.log(qr);//图片 base64的
-
-    });
-
-    var opts = {
-        beforeDrift: 2,
-        afterDrift: 2,
-        drift: 4,
-        step: 30
-    };
-
-    var counter = Math.floor(Date.now() / 1000 / opts.step);
-    var code = tfa.generateCode(key, counter);//用来登录的6位短码
-    console.log(code);
-});
--- webServer/config/serverConfig.js
+++ webServer/config/serverConfig.js
@@ -3,7 +3,7 @@
  */
 const fs = require('fs');
 const fun = require('../../nodeServer/function')
-const userserver=require('./../../nodeServer/userserver').function
+const userserver = require('./../../nodeServer/userserver').function
 var serverConfig = {
     port: "80",
     web_root: "./webServer/web",//根据主要运行的js文件 index.js 来确定从哪里开始的,而不是webserver服务的server.js 的文件位置
@@ -24,8 +24,9 @@ var serverConfig = {
                 response.setHeader('Set-Cookie', "init=true; expires= Fri, 31 Dec 9999 23:59:59 GMT;");
             }
             //重定向
-            response.writeHead(303, { 'Content-Type':'text/html',
-                'Server':'nodejs-v10.8.0_gs-webserver',
+            response.writeHead(303, {
+                'Content-Type': 'text/html',
+                'Server': 'nodejs-v10.8.0_gs-webserver',
                 'Location': path
             })
             response.end()
@@ -34,8 +35,8 @@ var serverConfig = {
             console.log("接收到一个post请求", postdata.toString())
             sendFiles(this.config.index_page, response);
         },
-        '/file':async (request, response, cookie, sendFiles, entireData) => {
-            let res={//wangEdit要求的数据回传格式
+        '/file': async (request, response, cookie, sendFiles, entireData) => {
+            let res = {//wangEdit要求的数据回传格式
                 // errno 即错误代码，0 表示没有错误。
                 //       如果有错误，errno != 0，可通过下文中的监听函数 fail 拿到该错误码进行自定义处理
                 errno: 0,
@@ -43,39 +44,64 @@ var serverConfig = {
                 data: []
             }
             function ranStr() {
-                return Math.random().toString(36).substring(2)+Math.random().toString(36).substring(2)+Math.random().toString(36).substring(2)
+                return Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2)
             }
             //保存文件
-            const files=fun.formFile(entireData)//解析二进制的数据
+            const files = fun.formFile(entireData)//解析二进制的数据
             for (let i = 0; i < files.length; i++) {
                 const element = files[i];
-                let path=await (new Promise((resolve,reject)=>{
-                    const tempPatn=ranStr()+"_"+element.describe.filename
-                        //TODO:这里的目录应该要再初始化模块中初始化
-                        //TODO:理论上来说如果有人构造恶意数据这里的filename直接拼接是一个严重的漏洞 比如将name 构造为 ../之类的
-                        fs.writeFile(process.cwd()+"/webServer/web/file/"+tempPatn, entireData.slice(element.start,element.end), 'binary', (err)=>{
-                            if(err)
+                let path = await (new Promise((resolve, reject) => {
+                    const tempPatn = ranStr() + "_" + element.describe.filename
+                    //TODO:这里的目录应该要再初始化模块中初始化
+                    //TODO:理论上来说如果有人构造恶意数据这里的filename直接拼接是一个严重的漏洞 比如将name 构造为 ../之类的
+                    fs.writeFile(process.cwd() + "/webServer/web/file/" + tempPatn, entireData.slice(element.start, element.end), 'binary', (err) => {
+                        if (err)
                             reject(err)
-                            else{
-                                //这个路径是基于web目录的TODO:应该按照config来配置
-                                resolve("./file/"+tempPatn)
-                            }
-                        });
-                    }))
+                        else {
+                            //这个路径是基于web目录的TODO:应该按照config来配置
+                            resolve("./file/" + tempPatn)
+                        }
+                    });
+                }))
                 res.data.push(path)
             }
             sendFiles(res, response);
         },
-        "/register"     :userserver["/register"],       //注册
-        "/login"        :userserver["/login"],          //登录
-        "/article"      :userserver["/article"],        //发布文章
-        "/getArticle"   :userserver["/getArticle"],     //获取文章
-        "/deleteArticle":userserver["/deleteArticle"],  //删除文章
-        '/updateArticle':userserver["/updateArticle"],  //修改文章
-        '/searchArticle':userserver['/searchArticle'],  //搜索文章
-        '/getComment'   :userserver['/getComment'],     //获取文章评论
-        '/addComment'   :userserver['/addComment'],     //提交评论
-        '/deleteComment':userserver['/deleteComment'],  //删除评论
+        "/register": userserver["/register"],       //注册
+        "/login": userserver["/login"],          //登录
+        "/article": userserver["/article"],        //发布文章
+        "/getArticle": userserver["/getArticle"],     //获取文章
+        "/deleteArticle": userserver["/deleteArticle"],  //删除文章
+        '/updateArticle': userserver["/updateArticle"],  //修改文章
+        '/searchArticle': userserver['/searchArticle'],  //搜索文章
+        '/getComment': userserver['/getComment'],     //获取文章评论
+        '/addComment': userserver['/addComment'],     //提交评论
+        '/deleteComment': userserver['/deleteComment'],  //删除评论
+        '/qr': function(request, response, cookie, sendFiles, data) {//get方法，返回一个qr码
+            var tfa = require('2fa');
+            tfa.generateKey(31, function (err, key) {
+                console.log(key);
+                tfa.generateGoogleQR('Company', 'email@gmail.com', key, function (err, qr) {
+                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                        var html= `<!DOCTYPE html>
+                        <html lang="zh">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                            <title>Document</title>
+                        </head>
+                        <body>
+                            <img src="${qr}">
+                        </body>
+                        </html>`
+                    response.end(html);
+                });
+                var counter = Math.floor(Date.now()/100/3);
+                var code = tfa.generateCode(key, counter);//用来登录的6位短码
+                console.log(code);
+            });
+        }
     }
 };
 exports.config = serverConfig;
